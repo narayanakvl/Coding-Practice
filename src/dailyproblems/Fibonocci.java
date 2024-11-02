9ecc6900826e6ec28be7f230594a736d368e57b6
narayanakvl <narayanakvl1122@gmail.com>
2024-11-02 23:10:11 +0530

    02-11-2024

---
+++ src/dailyproblems/Fibonocci.java
@@ -0,0 +1,16 @@
+package dailyproblems;
+
+public class Fibonocci {
+    public static void main(String[] args) {
+        int n=10;
+        int num1=0;
+        int num2=1;
+        System.out.print(num1+" "+num2);
+        for(int i=0;i<n;i++){
+            int temp= num1+num2;
+            System.out.print(" "+temp);
+            num1=num2;
+            num2=temp;
+        }
+    }
+}
