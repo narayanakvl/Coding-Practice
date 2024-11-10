25324000bd89f0cfd7cfa2465ac34a48700c9cd1
narayanakvl <narayanakvl1122@gmail.com>
2024-11-10 18:09:39 +0530

    10-11-2024

---
+++ src/striversheet/CountDigits.java
@@ -0,0 +1,19 @@
+package striversheet;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        //bruteforce approach
+        int n=12345;
+        int c=0;
+        while(n!=0){
+           n=n/10;
+           c++;
+        }
+        System.out.println("through bruteforce approach "+ c);
+        //optimal approach
+        int k=12345;
+        int r = (int) (Math.log10(k))+1;
+        System.out.println("through optimal approach "+ r);
+
+    }
+}
