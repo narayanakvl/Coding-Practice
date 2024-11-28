96fec41bd041bad9e8e826dbe0ec16f7fc5681f9
narayanakvl <narayanakvl1122@gmail.com>
2024-11-28 23:31:28 +0530

    28-11-2024

---
+++ src/striversheet/arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,26 @@
+package striversheet.arrays;
+
+public class MaxConsecutiveOnes {
+    public int getCount(int[] arr) {
+        int c = 0;
+        int mc = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==1){
+                c=c+1;
+            }else{
+                if(mc<c){
+                    mc=c;
+                    c=0;
+                }else{
+                    c=0;
+                }
+            }
+        }
+        return Math.max(mc, c);
+    }
+    public static void main(String[] args){
+        int[] arr= {1,1,0,1,0,1,1,1,1,0};
+        MaxConsecutiveOnes mco = new MaxConsecutiveOnes();
+        System.out.println(mco.getCount(arr));
+    }
+}
