3367f830797646a6fe602c1fc27ae946dc23a658
narayanakvl <narayanakvl1122@gmail.com>
2025-01-08 22:07:40 +0530

    08-01-2025

--- src/striversheet/binarysearch/FindRotations.java
+++ src/striversheet/binarysearch/FindRotations.java
@@ -26,6 +26,6 @@ public class FindRotations {
 
     public static void main(String[] args) {
         int[] arr={4,5,6,1,2,3};
-        System.out.println("Number is Rotations = "+FindRotations.findKRotations(arr));
+        System.out.println("Number of Rotations = "+FindRotations.findKRotations(arr));
     }
 }
