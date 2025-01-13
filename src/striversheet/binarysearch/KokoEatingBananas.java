4ef9250c1e75fd4cdc01b92ed86dc2fe68dac930
narayanakvl <narayanakvl1122@gmail.com>
2025-01-13 20:06:20 +0530

    13-01-2025

---
+++ src/striversheet/binarysearch/KokoEatingBananas.java
@@ -0,0 +1,37 @@
+package striversheet.binarysearch;
+
+public class KokoEatingBananas {
+    public static int findMax(int[] v) {
+        int maxi = Integer.MIN_VALUE;;
+        int n = v.length;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, v[i]);
+        }
+        return maxi;
+    }
+    public static int calculateTotalHours(int[] v, int hourly) {
+        int totalH = 0;
+        int n = v.length;
+        for (int i = 0; i < n; i++) {
+            totalH += Math.ceil((double)(v[i]) / (double)(hourly));
+        }
+        return totalH;
+    }
+    public static int minimumRateToEatBananas(int[] v, int h) {
+        int low = 1, high = findMax(v);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(v, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        int[] arr={7, 15, 6, 3};
+        System.out.println(minimumRateToEatBananas(arr,8));
+    }
+}
