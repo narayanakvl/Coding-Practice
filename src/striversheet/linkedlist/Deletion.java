4483b00e6ad8bb56f389aeb539e96fdb9fef544d
narayanakvl <narayanakvl1122@gmail.com>
2025-01-21 22:40:25 +0530

    21-01-2025

--- src/striversheet/linkedlist/Deletion.java
+++ src/striversheet/linkedlist/Deletion.java
@@ -14,14 +14,29 @@ public class Deletion {
         mover.next=null;
         return head;
     }
+    static Node deleteKthPosition(Node head,int k){
+        if(k==1){
+            return deleteHead(head);
+        }
+        Node mover=head;
+        int c=1;
+        while(c<k-1){
+            mover=mover.next;
+            c++;
+        }
+        mover.next=mover.next.next;
+        return head;
+    }
     public static void main(String[] args) {
-        int[] arr={1,2,3,4};
+        int[] arr={1,2,3,4,5,6};
         Node head=ConvertArr2LL.convert2LL(arr);
         Node newHead=new Node(100);
         Node presentHead=deleteHead(head);
         ConvertArr2LL.printLL(presentHead);
         Node newTail=new Node(101);
         deleteTail(presentHead);
+        ConvertArr2LL.printLL(presentHead);
+        deleteKthPosition(presentHead,2);
         ConvertArr2LL.printLL(presentHead);
     }
 }
--- src/striversheet/linkedlist/Insertion.java
+++ src/striversheet/linkedlist/Insertion.java
@@ -13,6 +13,20 @@ public class Insertion {
         mover.next=newTail;
         return head;
     }
+    static Node insertKthPosition(Node head,Node newNode,int k){
+        if(k==1){
+            return insertHead(head,newNode);
+        }
+        Node mover=head;
+        int c=1;
+        while(c<k-1){
+            mover=mover.next;
+            c++;
+        }
+        newNode.next=mover.next;
+        mover.next=newNode;
+        return head;
+    }
     public static void main(String[] args) {
         int[] arr={1,2,3,4};
         Node head=ConvertArr2LL.convert2LL(arr);
@@ -21,6 +35,9 @@ public class Insertion {
         ConvertArr2LL.printLL(presentHead);
         Node newTail=new Node(101);
         insertTail(presentHead,newTail);
+        ConvertArr2LL.printLL(presentHead);
+        Node newNode=new Node(103);
+        insertKthPosition(presentHead,newNode,2);
         ConvertArr2LL.printLL(presentHead);
     }
 }
