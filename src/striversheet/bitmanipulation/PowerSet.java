eb8ed0ab5d685cdfdbc411c8b1b53cfb4c23a723
narayanakvl <narayanakvl1122@gmail.com>
2025-02-19 23:15:42 +0530

    19-02-2025

---
+++ src/striversheet/bitmanipulation/PowerSet.java
@@ -0,0 +1,24 @@
+package striversheet.bitmanipulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerSet {
+    static List<List<Integer>> powerSet(int[] arr) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int i = 0; i <= Math.pow(2,arr.length)-1; i++) {
+            List<Integer> lt=new ArrayList<>();
+            for (int j = 0; j < arr.length; j++) {
+                if((i & (1<<j))!=0){
+                    lt.add(arr[j]);
+                }
+            }
+            res.add(lt);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,4,3};
+        System.out.println(powerSet(arr));
+    }
+}
