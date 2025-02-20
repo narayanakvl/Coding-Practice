1aebe1033f402f687d01967def95689faed36845
narayanakvl <narayanakvl1122@gmail.com>
2025-02-20 23:54:11 +0530

    20-02-2025

---
+++ src/striversheet/bitmanipulation/XorOfNumbers.java
@@ -0,0 +1,21 @@
+package striversheet.bitmanipulation;
+
+public class XorOfNumbers {
+    static int xorTillN(int n){
+        int xor=0;
+        if(n%4==0){
+            return n;
+        }else if(n%4==1){
+            return 0;
+        }else if(n%4==2){
+            return n-1;
+        }else if(n%4==3){
+            return 0;
+        }else{
+            return n;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(xorTillN(9));
+    }
+}
