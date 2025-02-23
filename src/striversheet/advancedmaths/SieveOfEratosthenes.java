bf0536490d8df488f3321341e0ad713124cb0ef9
narayanakvl <narayanakvl1122@gmail.com>
2025-02-23 23:49:30 +0530

    23-02-2025

---
+++ src/striversheet/advancedmaths/SieveOfEratosthenes.java
@@ -0,0 +1,26 @@
+package striversheet.advancedmaths;
+
+public class SieveOfEratosthenes {
+    static void printPrime(int n) {
+        int[] arr = new int[n+1];
+        for (int i = 2; i <= n; i++) {
+            arr[i] = 1;
+        }
+        for (int i = 2; i*i <= n; i++) {
+            if(arr[i] == 1){
+                for (int j = i * i; j <= n; j += i) {
+                    arr[j] = 0;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if(arr[i] == 1){
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int n=30;
+        printPrime(n);
+    }
+}
